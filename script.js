c9bdc9ec3425967ef23151e2eb477ec45925f71a
JaviIG21 <adolfokataboom21@gmail.com>
2024-11-07 07:27:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.getElementById('clienteForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Previene la recarga de la página
+    event.preventDefault();
 
-    // Captura los datos del formulario
+
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const correo = document.getElementById('correo').value;
     const telefono = document.getElementById('telefono').value;
 
-    // Enviar los datos a la API
     fetch('http://localhost:3000/clientes', {
         method: 'POST',
         headers: {
@@ -17,13 +16,11 @@ document.getElementById('clienteForm').addEventListener('submit', function(event
     })
     .then(response => response.json())
     .then(data => {
-        // Mostrar mensaje al usuario
+
         document.getElementById('message').textContent = data.message;
 
-        // Limpiar el formulario
         document.getElementById('clienteForm').reset();
 
-        // Actualizar la lista de clientes
         obtenerClientes();
     })
     .catch(error => {
@@ -31,13 +28,13 @@ document.getElementById('clienteForm').addEventListener('submit', function(event
     });
 });
 
-// Función para obtener la lista de clientes
+
 function obtenerClientes() {
     fetch('http://localhost:3000/clientes')
     .then(response => response.json())
     .then(data => {
         const clientesList = document.getElementById('clientesList');
-        clientesList.innerHTML = ''; // Limpiar la lista actual
+        clientesList.innerHTML = '';
 
         data.forEach(cliente => {
             const clienteItem = document.createElement('div');
@@ -49,5 +46,4 @@ function obtenerClientes() {
     .catch(error => console.error('Error:', error));
 }
 
-// Llamar a obtenerClientes al cargar la página para mostrar clientes existentes
 obtenerClientes();
