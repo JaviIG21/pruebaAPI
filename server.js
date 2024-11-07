7777f189e1b23dc2bdfab024d1ceb90e2ce4bf0d
JaviIG21 <adolfokataboom21@gmail.com>
2024-11-07 08:08:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,26 +6,21 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Conexión a la base de datos
 const db = mysql.createConnection({
-    host: 'localhost',   // Asegúrate que este es tu host de MySQL
-    user: 'root',        // Usuario de MySQL
-    password: '',        // Deja vacío si no hay contraseña
-    database: 'usuarios' // Nombre de la base de datos
+    host: 'localhost',   
+    user: 'root',      
+    password: '',        
+    database: 'usuarios'
 });
 
-// Conectar a la base de datos
 db.connect((err) => {
     if (err) throw err;
     console.log('Conectado a la base de datos!');
 });
 
-// Rutas CRUD
-// Crear un nuevo cliente
 app.post('/clientes', (req, res) => {
     const cliente = req.body;
     const query = 'INSERT INTO clientes SET ?';
@@ -38,7 +33,6 @@ app.post('/clientes', (req, res) => {
     });
 });
 
-// Leer todos los clientes
 app.get('/clientes', (req, res) => {
     db.query('SELECT * FROM clientes', (err, results) => {
         if (err) {
@@ -49,7 +43,6 @@ app.get('/clientes', (req, res) => {
     });
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
     console.log(`API escuchando en http://localhost:${port}`);
 });
